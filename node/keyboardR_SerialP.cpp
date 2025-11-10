3940b22183945d11918eaa663823a5ec8579d8be
nano5555 <sanyovippro@gmail.com>
2025-11-10 19:20:32 +0000

    initial ros setup

---
+++ node/keyboardR_SerialP.cpp
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+dda
+/* This example creates a subclass of Node and uses a fancy C++11 lambda
+* function to shorten the callback syntax, at the expense of making the
+* code somewhat more difficult to understand at first glance. */
+
+class SerialP : public rclcpp::Node
+{
+public:
+
+
+private:
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(SerialP);
+  rclcpp::shutdown();
+  return 0;
+}
